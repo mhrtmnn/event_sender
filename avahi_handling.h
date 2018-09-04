1199e35da0949e7e0ab21c6f7e15c28f50243ef1
Marco Hartmann <marcohartmann95@gmail.com>
2018-09-04 02:39:31 +0200

    Add consistency

--- avahi_handling.h
+++ avahi_handling.h
@@ -1,6 +1,7 @@
 #ifndef _avahi_handling
 #define _avahi_handling
 
+
 /*******************************************************************************
 * PROTOTYPES
 *******************************************************************************/
@@ -11,5 +12,6 @@
  * return: 0 on success, <0 on error
  */
 int avahi_find_host_addr(char **, unsigned *);
+
 
 #endif /* _avahi_handling */
